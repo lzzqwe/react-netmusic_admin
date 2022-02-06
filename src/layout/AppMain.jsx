2c065ed1c374af893b5736650076760a3e1f5fdc
lzzqwe <352840359@qq.com>
2022-02-06 21:08:28 +0800

    优化了若干

--- src/layout/AppMain.jsx
+++ src/layout/AppMain.jsx
@@ -1,21 +1,28 @@
 import './AppMain.scss'
-import * as React from "react";
-import { Routes, Route, } from "react-router-dom";
-import Home from '../views/Home';
-import User from '../views/User'
-import SelectedDisk from '../views/SelectedDisk';
-import HighQualitySonglist from '../views/HighQualitySonglist'
-import Singer from '../views/Singer'
+import React, {Suspense, lazy} from "react";
+import {Routes, Route,} from "react-router-dom";
+import {Spin} from 'antd'
+
+const Home = lazy(() => import('../views/Home'))
+const User = lazy(() => import('../views/User'))
+const SelectedDisk = lazy(() => import('../views/SelectedDisk'))
+const HighQualitySonglist = lazy(() => import('../views/HighQualitySonglist'))
+const Singer = lazy(() => import('../views/Singer'))
 const AppMain = () => {
     return (
         <div className="app-main">
-            <Routes>
-                <Route path="/home" element={<Home />} />
-                <Route path="/user" element={<User />} />
-                <Route path="/selectedDisk" element={<SelectedDisk />} />
-                <Route path="/highQualitySonglist" element={<HighQualitySonglist />} />
-                <Route path="/singer" element={<Singer />} />
-            </Routes>
+            <Suspense fallback={<div className='app-main-loading'>
+                <Spin></Spin>
+            </div>}>
+                <Routes>
+                    <Route path="/home" element={<Home/>}/>
+                    <Route path="/user" element={<User/>}/>
+                    <Route path="/selectedDisk" element={<SelectedDisk/>}/>
+                    <Route path="/highQualitySonglist" element={<HighQualitySonglist/>}/>
+                    <Route path="/singer" element={<Singer/>}/>
+                </Routes>
+            </Suspense>
+
         </div>
     )
 }
